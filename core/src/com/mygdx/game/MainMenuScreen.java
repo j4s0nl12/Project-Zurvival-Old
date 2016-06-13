7bba2cfbf28735bfcfc17681b53838883cfd7b80
Kryxem <jajli@ucsc.edu>
2016-06-13 15:57:39 -0700

    Added more images and "Buttons"

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -6,19 +6,43 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MainMenuScreen implements Screen{
 	
 	final Zurvival game;
 	OrthographicCamera camera;
+	
+	//Images
 	Texture titleImg;
+	Texture newgameImg;
+	Texture continueImg;
+	Texture optionsImg;
+	Texture statImg;
+	
+	//Image bounds
+	Rectangle newgameBound;
+	Rectangle continueBound;
+	Rectangle optionsBound;
+	Rectangle statBound;
 	
 	MainMenuScreen(final Zurvival gam){
 		game = gam;
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());//Look at Desktop Launcher for Desktop Resolution
 		
-		titleImg = new Texture("Menus/Title.png");
+		titleImg = new Texture("Images/Menus/Title.png");
+		newgameImg = new Texture("Images/Menus/New Game.png");
+		continueImg = new Texture("Images/Menus/Continue.png");
+		optionsImg = new Texture("Images/Menus/Options.png");
+		statImg = new Texture("Images/Menus/Statistics.png");
+		
+		newgameBound = new Rectangle(Gdx.graphics.getWidth()/2 - newgameImg.getWidth()/2, 300, newgameImg.getWidth(), newgameImg.getHeight());
+		continueBound = new Rectangle(Gdx.graphics.getWidth()/2 - continueImg.getWidth()/2, 300, continueImg.getWidth(), continueImg.getHeight());
+		optionsBound = new Rectangle(Gdx.graphics.getWidth()/2 - optionsImg.getWidth()/2, 200, optionsImg.getWidth(), optionsImg.getHeight());
+		statBound = new Rectangle(Gdx.graphics.getWidth()/2 - statImg.getWidth()/2, 100, statImg.getWidth(), statImg.getHeight());
+		
 	}
 
 	@Override
@@ -40,7 +64,36 @@ public class MainMenuScreen implements Screen{
 		
 		game.batch.begin();
 		game.batch.draw(titleImg, Gdx.graphics.getWidth()/2 - titleImg.getWidth()/2, Gdx.graphics.getHeight() - 350);
+		game.batch.draw(newgameImg, newgameBound.getX(), 300);
+		//game.batch.draw(continueImg, continueBound.getX(), 300);
+		game.batch.draw(optionsImg, optionsBound.getX(), 200);
+		game.batch.draw(statImg, statBound.getX(), 100);
 		game.batch.end();
+		
+		if(Gdx.input.isTouched()){
+			Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(),0);
+			camera.unproject(touchPos);
+			
+			//New Game
+			if(newgameBound.contains(touchPos.x,touchPos.y)){
+				
+			}
+			
+			//Continue
+			if(continueBound.contains(touchPos.x, touchPos.y)){
+				
+			}
+			
+			//Options
+			if(optionsBound.contains(touchPos.x, touchPos.y)){
+				
+			}
+			
+			//Statistics
+			if(statBound.contains(touchPos.x, touchPos.y)){
+				
+			}
+		}
 	}
 
 	@Override
