a9337233ced9fabe9a555cb4edc9e9a78aa04d94
Robin Chen <robin3chen@gmail.com>
2016-06-14 15:20:10 -0700

    texture packer, box2d
    box2d was barely touched

--- core/src/com/mygdx/game/Screens/RobinFunsiesScreen.java
+++ core/src/com/mygdx/game/Screens/RobinFunsiesScreen.java
@@ -5,8 +5,25 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.mygdx.game.Game.Zurvival;
+
+import java.util.HashMap;
 
 /**
  * Created by robin on 6/13/2016.
@@ -16,23 +33,76 @@ public class RobinFunsiesScreen implements Screen {
     final Zurvival mGame;
     OrthographicCamera camera;
     ParticleEffect pe;
+    TextureAtlas textureAtlas;
+    SpriteBatch batch;
+    ExtendViewport viewport;
+
+    static final float TIME_STEP = 1f / 60f;
+    static final int VELOCITY_ITERATIONS = 6;
+    static final int POSITION_ITERATIONS = 2;
+
+    final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>();
+    World world;
+    Box2DDebugRenderer debugRenderer;
+    Array<Body> bodies = new Array<Body>();
+
+    private float accumulator = 0;
+
+    private void doPhysicsStep(float deltaTime) {
+        // fixed time step
+        // max frame time to avoid spiral of death (on slow devices)
+        float frameTime = Math.min(deltaTime, 0.25f);
+        accumulator += frameTime;
+        while (accumulator >= TIME_STEP) {
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+            accumulator -= TIME_STEP;
+        }
+    }
 
     public RobinFunsiesScreen(final Zurvival game){
         mGame = game;
+    }
+
+    @Override
+    public void show() {
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer = new Box2DDebugRenderer();
+
+        batch = new SpriteBatch();
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        viewport = new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+
+        textureAtlas = new TextureAtlas("sprites-packed/pack.atlas");
+
+        addSprites();
 
         pe = new ParticleEffect();
         pe.load(Gdx.files.internal("Images/Particles/BulletFire.particle"),  Gdx.files.internal("Images/Particles/"));
         pe.getEmitters().first().setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
         pe.start();
-    }
 
-    @Override
-    public void show() {
-        // TODO Auto-generated method stub
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(100, 300);
+        Body body = world.createBody(bodyDef);
+        body.setUserData("orange");
 
+        CircleShape shape = new CircleShape();
+        shape.setRadius(63f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        shape.dispose();
+
+        world.getBodies(bodies);
     }
 
     @Override
@@ -40,22 +110,40 @@ public class RobinFunsiesScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        SpriteBatch batch = mGame.batch;
-
         batch.begin();
+
         pe.update(Gdx.graphics.getDeltaTime());
         pe.draw(batch);
+
+        drawSprite("banana", 0, 0);
+        drawSprite("cherries", 100, 100);
+
+        for (Body b : bodies) {
+            // Get the body's user data - in this example, our user
+            // data is an instance of the Entity class
+            if (b != null) {
+                String key = (String) b.getUserData();
+                Sprite sprite = sprites.get(key);
+                drawSprite(key, b.getPosition().x - sprite.getWidth()/2, b.getPosition().y - sprite.getHeight()/2);
+            }
+        }
+
         batch.end();
 
         if(pe.isComplete()){
             pe.reset();
         }
+        debugRenderer.render(world, camera.combined);
+
+        // recommended at end of render
+        doPhysicsStep(delta);
     }
 
     @Override
     public void resize(int width, int height) {
-        // TODO Auto-generated method stub
+        viewport.update(width, height, true);
 
+        batch.setProjectionMatrix(camera.combined);
     }
 
     @Override
@@ -78,5 +166,25 @@ public class RobinFunsiesScreen implements Screen {
 
     @Override
     public void dispose() {
+        textureAtlas.dispose();
+        world.dispose();
+    }
+
+    private void drawSprite(String name, float x, float y) {
+        Sprite sprite = sprites.get(name);
+
+        sprite.setPosition(x, y);
+
+        sprite.draw(batch);
+    }
+
+    private void addSprites() {
+        Array<TextureAtlas.AtlasRegion> regions = textureAtlas.getRegions();
+
+        for (TextureAtlas.AtlasRegion region : regions) {
+            Sprite sprite = textureAtlas.createSprite(region.name);
+
+            sprites.put(region.name, sprite);
+        }
     }
 }
